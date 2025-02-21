7e8f10d9970a3a35cdaaa54ca460ead6896f226f
Shlimazl31 <semh.blut@gmail.com>
2025-02-21 03:16:30 +0300

    getPlayer fonksiyonu tekrarlanma hatası düzeltildi

--- src/utils/player.js
+++ src/utils/player.js
@@ -2,31 +2,8 @@ import { Player } from 'discord-player';
 import { DefaultExtractors } from '@discord-player/extractor';
 import { GatewayIntentBits } from 'discord.js';
 
-let player = null;
-
-const getPlayer = async (client) => {
+export const getPlayer = async (client) => {
     const player = new Player(client);
-    
-    // Eski kod:
-    // await player.extractors.loadDefault();
-    
-    // Yeni kod:
     await player.extractors.loadMulti(DefaultExtractors);
-    
-    return player;
-}
-
-export async function getPlayer(client) {
-    if (!player) {
-        player = new Player(client, {
-            ytdlOptions: {
-                quality: 'highestaudio',
-                highWaterMark: 1 << 25
-            }
-        });
-        
-        // Yeni extractor yükleme yöntemi
-        await player.extractors.loadMulti(DefaultExtractors);
-    }
     return player;
 }
